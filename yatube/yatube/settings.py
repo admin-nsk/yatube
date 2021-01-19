233a16f352b5ef2ccd1964640433e17b98b7894d
admin-nsk <66418181+admin-nsk@users.noreply.github.com>
2021-01-19 16:52:15 +0700

    Create form password_change. 4 sprint 15 topic 16 lesson

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -126,3 +126,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# Login
+
+LOGIN_URL = "/auth/login/"
+LOGIN_REDIRECT_URL = "index"
+# LOGOUT_REDIRECT_URL = "index"
+EMAIL_BACKEND = 'django.core.mail.backends.XXX'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
