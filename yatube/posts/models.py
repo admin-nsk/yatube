8bc21ee6d622b8f5d4e725e6accc539703dfc83d
admin-nsk <66418181+admin-nsk@users.noreply.github.com>
2021-02-10 11:28:37 +0700

    Add test image in post

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,4 @@
+from django.core import validators
 from django.db import models
 from django.contrib.auth import get_user_model
 from group.models import Group
@@ -10,7 +11,13 @@ class Post(models.Model):
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, related_name='posts', blank=True)
-    image = models.ImageField(upload_to='posts/', blank=True, null=True, verbose_name='Изображение')
+    image = models.ImageField(
+        upload_to='posts/',
+        blank=True,
+        null=True,
+        verbose_name='Изображение',
+        validators=[validators.validate_image_file_extension],
+        error_messages={'invalid_extension': 'Этот формат файлов не поддерживается'})
 
     def __str__(self):
         return self.text
--- yatube/tests/test_posts.py
+++ yatube/tests/test_posts.py
@@ -9,25 +9,36 @@ class TestPosts(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username='testuser', email='testuser@mail.com', password='12345')
+        self.test_username = 'testuser'
+        self.test_email = 'testuser@mail.com'
+        self.test_password = '12345'
+        self.user = User.objects.create_user(
+            username=self.test_username,
+            email=self.test_email,
+            password=self.test_password)
         self.guest = User.objects.create_user(username='guest', email='guest@mail.com', password='54321')
         self.post = Post.objects.create(
             text='Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. '
                  'Lorem Ipsum используют потому, что тот обеспечивает более или менее',
-            author=self.user
-        )
+            author=self.user,
+            image='test-img.jpg'
+            )
+        self.post_id = self.post.id
 
     def test_profile(self):
-        self.client.login(username='testuser', password='12345')
+        self.client.login(username=self.test_username, password=self.test_password)
         response = self.client.get(f'/{self.user.username}/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context["page"]), 1)
         self.assertIsInstance(response.context["author"], User)
         self.assertEqual(response.context["author"].username, self.user.username)
+        text_img = "<img "
+        self.assertContains(response, text_img, status_code=200)
 
     def test_create_post(self):
-        if self.client.login(username='testuser', password='12345', follow=True):
-            response = self.client.post('/new/', {'text': 'test post for testuser'}, follow=True)
+        if self.client.login(username=self.test_username, password=self.test_password, follow=True):
+            with open('media/test-img.jpg', 'rb') as img:
+                response = self.client.post('/new/', {'text': 'test post for testuser', 'image': img}, follow=True)
             self.assertEqual(response.status_code, 200)
 
     def test_fail_create_post(self):
@@ -44,11 +55,13 @@ class TestPosts(TestCase):
                 reverse('post', kwargs={'username': self.user.username, 'post_id': post.id})
             ):
                 response = self.client.get(url)
+                text_img = "<img "
+                self.assertContains(response, text_img, status_code=200)
                 self.assertContains(response, post.text)
 
     def test_edit_post(self):
         posts = self.user.posts.all()
-        if self.client.login(username='testuser', password='12345', follow=True):
+        if self.client.login(username=self.test_username, password=self.test_password, follow=True):
             for post in posts:
                 response = self.client.post(
                     reverse('post_edit', kwargs=
@@ -65,3 +78,20 @@ class TestPosts(TestCase):
             ):
                 response = self.client.get(url)
                 self.assertContains(response, post.text)
+
+    def test_post_have_img(self):
+        post = self.user.posts.first()
+        response = self.client.get(reverse('post', kwargs={'username': self.user.username, 'post_id': self.post_id}))
+        text_img = "<img "
+        self.assertContains(response, text_img, status_code=200)
+
+    def test_load_no_image(self):
+        post = self.user.posts.first()
+        if self.client.login(username=self.test_username, password=self.test_password, follow=True):
+            with open('media/test-load.js', 'rb') as img:
+                response = self.client.post(
+                    reverse('post_edit', kwargs=
+                    {'username': self.user.username,
+                     'post_id': post.id,
+                     }), {'text': 'Modify text in the post', 'image': img}, follow=True)
+            self.assertContains(response, 'errorlist', status_code=200)
