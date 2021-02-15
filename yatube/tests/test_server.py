1f9ee41623346d50f7359ae1d95c0664a2b7f556
admin-nsk <66418181+admin-nsk@users.noreply.github.com>
2021-02-15 21:45:03 +0700

    Add test subscribe, comments. Install DjDT apps

--- yatube/tests/test_server.py
+++ yatube/tests/test_server.py
@@ -1,14 +1,64 @@
 from django.test import TestCase, Client
-
-
+from django.urls import reverse
+from posts.models import Post, User
 
 class TestServer(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.test_username1 = 'testuser1'
+        self.test_username2 = 'testuser2'
+        self.test_email = 'testuser@mail.com'
+        self.test_password = '12345'
+        self.user1 = User.objects.create_user(
+            username=self.test_username1,
+            email=self.test_email,
+            password=self.test_password)
+        self.user2 = User.objects.create_user(
+            username=self.test_username2,
+            email=self.test_email,
+            password=self.test_password)
+        self.guest = User.objects.create_user(username='guest', email='guest@mail.com', password='54321')
+        self.post = Post.objects.create(
+            text='Тестовый пост первого пользователя',
+            author=self.user1,
+            image='test-img.jpg'
+        )
+        self.post_two = Post.objects.create(
+            text='Тестовый пост второго пользователя',
+            author=self.user2,
+            image='test-img.jpg'
+        )
+        self.post_id = self.post.id
 
     def test_404(self):
         response = self.client.get('/last/')
         self.assertEqual(response.status_code, 404)
 
+    def test_auth_user_subscribe(self):
+        if self.client.login(username=self.test_username1, password=self.test_password, follow=True):
+            response = self.client.get(f'/{self.user2.username}/')
+            self.assertEqual(response.status_code, 200)
+            response = self.client.post(f'/{self.user2.username}/follow/', username=self.user2.username, follow=True)
+            self.assertEqual(response.status_code, 200)
+            response = self.client.post(f'/{self.user2.username}/unfollow/', username=self.user2.username, follow=True)
+            self.assertEqual(response.status_code, 200)
+
+    def test_post_following_user(self):
+        if self.client.login(username=self.test_username1, password=self.test_password, follow=True):
+            response = self.client.get('/follow/')
+            self.assertNotContains(response, 'Тестовый пост второго пользователя')
+            response = self.client.post(f'/{self.user2.username}/follow/', username=self.user2.username, follow=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, 'Тестовый пост второго пользователя')
+
+    def test_create_comments(self):
+        self.client.login(username='test', password='12345', follow=True)
+        response = self.client.post(f'/{self.user1.username}/{self.post.id}/comment', {'text': 'test comment'}, follow=True)
+        self.assertNotContains(response, 'test comment')
+        if self.client.login(username=self.test_username1, password=self.test_password, follow=True):
+            response = self.client.post(f'/{self.user1.username}/{self.post.id}/comment', {'text': 'test comment'},
+                                        follow=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, 'test comment')
 
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -19,9 +19,11 @@ from django.urls import path, include
 from django.conf.urls import handler404, handler500
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
 
 handler404 = "posts.views.page_not_found" # noqa
 handler500 = "posts.views.server_error" # noqa
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -40,3 +42,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
